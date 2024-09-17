3703c897bcfa3b67f94c9f654abfc90402e2d50a
xyl_t9 <dbsrjs1224@gmail.com>
2024-09-17 23:39:52 +0900

    [2024-09-17] PlayerPrefs bug fix

--- Assets/0.Scripts/Menu.cs
+++ Assets/0.Scripts/Menu.cs
@@ -1,34 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
-    public GameObject mainMenu;     //메인 메뉴 오브젝트
-    public GameObject optionMenu;   //옵션 메뉴 오브젝트
 
-    public Slider[] volumeSliders;       //볼륨 슬라이더
-    public Toggle[] resolutionToggles;   //해상도 토글
-    public Toggle fullscreenToggle;
-    public int[] screenWidths;           //해상도
+    public GameObject mainMenuHolder;       //메인 메뉴 오브젝트
+    public GameObject optionsMenuHolder;    //옵션 메뉴 오브젝트
 
-    int activeScreenResIndex;   //사용 해상도
+    public Slider[] volumeSliders;           //볼륨 슬라이더
+    public Toggle[] resolutionToggles;       //해상도 토글
+    public Toggle fullscreenToggle;          //전체 화면 토글
+    public int[] screenWidths;               //해상도
+    int activeScreenResIndex;                //사용 해상도 인덱스
 
-    private void Start()
+    void Start()
     {
         activeScreenResIndex = PlayerPrefs.GetInt("screen res index");
-        bool isFullscreen = (PlayerPrefs.GetInt("fullscreen") ==1 ) ? true : false;
+        bool isFullscreen = (PlayerPrefs.GetInt("fullscreen") == 1) ? true : false;
 
-        volumeSliders[0].value = AudioManager.instance.masterVolumePercent;
-		volumeSliders[1].value = AudioManager.instance.musicVolumePercent;
-		volumeSliders[2].value = AudioManager.instance.sfxVolumePercent;
-
-        for(int i = 0; i < resolutionToggles.Length; i++)
+        for (int i = 0; i < resolutionToggles.Length; i++)
         {
             resolutionToggles[i].isOn = i == activeScreenResIndex;
         }
+
+        if (isFullscreen)
+        {
+            for (int i = 0; i < resolutionToggles.Length; i++)
+            {
+                resolutionToggles[i].interactable = !isFullscreen;
+            }
+        }
+
+        volumeSliders[0].value = AudioManager.instance.masterVolumePercent;
+        volumeSliders[1].value = AudioManager.instance.musicVolumePercent;
+        volumeSliders[2].value = AudioManager.instance.sfxVolumePercent;
 
         fullscreenToggle.isOn = isFullscreen;
     }
@@ -46,40 +53,46 @@ public class Menu : MonoBehaviour
     /// </summary>
     public void Quit()
     {
-        #if UNITY_EDITOR
+#if UNITY_EDITOR
         // 이 코드는 Unity 에디터에서만 실행됨
         UnityEditor.EditorApplication.isPlaying = false;    //빌드 전 게임 종료
-        #endif
+#endif
+
         Application.Quit();
     }
 
     /// <summary>
-    /// 옵션 화면 활성화
+    /// 옵션 메뉴 활성화
     /// </summary>
-    public void OptionMenu()
+    public void OptionsMenu()
     {
-        mainMenu.SetActive(false);
-        optionMenu.SetActive(true);
+        mainMenuHolder.SetActive(false);
+        optionsMenuHolder.SetActive(true);
     }
 
     /// <summary>
-    /// 메인 화면 활성화
+    /// 메인 메뉴 활성화
     /// </summary>
     public void MainMenu()
     {
-        mainMenu.SetActive(true);
-        optionMenu.SetActive(false);
+        mainMenuHolder.SetActive(true);
+        optionsMenuHolder.SetActive(false);
     }
 
     /// <summary>
     /// 해상도 변경
     /// </summary>
-    public void SetScrrenResolution(int i)
+    /// <param name="i">해상도 인덱스</param>
+    public void SetScreenResolution(int i)
     {
+        // 선택한 해상도 토글이 켜져 있는지 확인
         if (resolutionToggles[i].isOn)
         {
+            activeScreenResIndex = i;   // 현재 해상도 인덱스를 업데이트
             float aspectRatio = 16 / 9f;
-            Screen.SetResolution(screenWidths[i], (int)(screenWidths[i] / aspectRatio), false); //해상도 변경, 전체 화면이 아님
+
+            // 주어진 해상도와 비율로 화면 해상도 설정, 전체 화면이 아님
+            Screen.SetResolution(screenWidths[i], (int)(screenWidths[i] / aspectRatio), false);
             PlayerPrefs.SetInt("screen res index", activeScreenResIndex);
             PlayerPrefs.Save();
         }
@@ -88,32 +101,38 @@ public class Menu : MonoBehaviour
     /// <summary>
     /// 풀스크린 설정
     /// </summary>
+    /// <param name="isFullscreen">전체 화면 여부</param>
     public void SetFullscreen(bool isFullscreen)
     {
-        for(int i = 0; i < resolutionToggles.Length; i++)
+        // 전체 화면 여부에 따라 해상도 토글의 활성화 여부 설정
+        for (int i = 0; i < resolutionToggles.Length; i++)
         {
-            activeScreenResIndex = i;     // 활성화된 해상도 인덱스를 현재 인덱스로 설정
-            resolutionToggles[i].interactable = !isFullscreen;   // 전체 화면이 아니면 토글을 활성화, 전체 화면이면 비활성화
+            resolutionToggles[i].interactable = !isFullscreen;
         }
 
-        if(isFullscreen)
+        if (isFullscreen)
         {
-            Resolution[] allResolutions = Screen.resolutions;       //모든 해상도 갖고 오기
-            Resolution maxResolution = allResolutions[allResolutions.Length - 1]; //가장 큰 해상도 선정
-            Screen.SetResolution(maxResolution.width, maxResolution.height, true); //가장 큰 해상도로 전체 화면 설정
+            // 모든 지원되는 해상도 목록을 가져옴
+            Resolution[] allResolutions = Screen.resolutions;
+            // 가장 큰 해상도를 선택
+            Resolution maxResolution = allResolutions[allResolutions.Length - 1];
+            // 선택한 해상도로 전체 화면 설정
+            Screen.SetResolution(maxResolution.width, maxResolution.height, true);
         }
         else
         {
-            SetScrrenResolution(activeScreenResIndex);  //이전에 활성화 했던 해상도로 변경
+            // 전체 화면을 해제하면 이전에 선택한 해상도로 복원
+            SetScreenResolution(activeScreenResIndex);
         }
 
-        PlayerPrefs.SetInt("fullscreen", isFullscreen ? 1 : 0);
+        PlayerPrefs.SetInt("fullscreen", ((isFullscreen) ? 1 : 0));
         PlayerPrefs.Save();
     }
 
     /// <summary>
     /// 마스터 볼륨 설정
     /// </summary>
+    /// <param name="value">마스터 볼륨 값</param>
     public void SetMasterVolume(float value)
     {
         AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Master);
@@ -122,16 +141,19 @@ public class Menu : MonoBehaviour
     /// <summary>
     /// 음악 볼륨 설정
     /// </summary>
+    /// <param name="value">음악 볼륨 값</param>
     public void SetMusicVolume(float value)
     {
         AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Music);
     }
 
     /// <summary>
-    /// 효과 볼륨 설정
+    /// 효과음 볼륨 설정
     /// </summary>
+    /// <param name="value">효과음 볼륨 값</param>
     public void SetSfxVolume(float value)
     {
         AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Sfx);
     }
+
 }
